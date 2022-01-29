673a86e72883463383bf7258b1d8e1848e866f7c
garlife <garlife@gmail.com>
2022-01-29 13:53:16 +0300

    forms p2

--- src/App.js
+++ src/App.js
@@ -1,62 +1,121 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select"
 
-export default function App() {
-  const [person, setPerson] = useState({
-    firstName: 'David',
-    secondName: 'Fisherman',
-    isFemale: false,
-    age: 0,
-    favouriteFruit: ''
-  });
+const user = {
+  firstName: "",
+  secondName: "",
+  age: 0,
+  isFemale: "",
+  favouriteFruit: "",
+};
 
-  console.log('App', person);
+function App() {
+  const [person, setPerson] = useState(user);
 
-  function handlerPerson(event) {
-    setPerson(event.target.value);
-  }
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+    control
+  } = useForm({
+    defaultValues: {
+      firstName: "Ann",
+    },
+  }); //!!!
 
-  function handlerChange(event) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    //setPerson(e.target.value);
+    console.log(e);
+  };
+
+  function handleChange(event) {
     const { name, value } = event.target;
-    const newPerson = {...person};
+    const newPerson = { ...person };
     newPerson[name] = value;
     setPerson(newPerson);
   }
 
+  console.log(
+    watch("firstName"),
+    watch("secondName"),
+    watch("age"),
+    watch("isFemale"),
+    watch("favouriteFruit"),
+    errors
+  );
+
   return (
-    <>
-      <form onSubmit={handlerPerson}>
-        <label>firstName</label>
-        <input type="text"
-          name="firstName"
-          value={person.firstName}
-          onChange={handlerChange}
-        />
-        <br />
-        <label>secondName</label>
-        <input type="text"
-          name="secondName"
-          value={person.secondName}
-          onChange={handlerChange}
-        />
-        <br />
-        <label>Age</label>
-        <input type="text" name="age" value={person.age} onChange={handlerChange} />
-        <br />
-        <label>isFemale</label>
-        <input type="checkbox" name="isFemale" value={person.isFemale} onChange={handlerChange} />
-        <br />
+    <div className="App">
+      <h1></h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          Name
+          <input
+            type="text"
+            {...register("firstName", {
+              required: true,
+              pattern: /^[А-Я][а-я]+$/i,
+            })}
+          />
+          {errors.firstName && (
+            <span> This field is required/pattern violation </span>
+          )}
+        </label>
+        <label>
+          Surname
+          <input
+            type="text"
+            {...register("secondName", { required: true, maxLength: 31 })}
+          ></input>
+          {errors.secondName && <span> This field is required! </span>}
+        </label>
+        <label>
+          Age
+          <input
+            type="number"
+            {...register("age", { min: 5, max: 100 })}
+          ></input>
+          {errors.number && <span>min/max err</span>}
+        </label>
+        <label>
+          Female
+          <input
+            type="checkbox"
+            {...register("isFemale", { required: true })}
+          ></input>
+        </label>
         <label>
           любимый фрукт:
-          <select value={person.value} onChange={handlerChange}>
+          {/* <select {...register("favouriteFruit")}>
             <option value="grapefruit">Грейпфрут</option>
             <option value="lime">Лайм</option>
-            
+
             <option value="mango">Манго</option>
-          </select>
+          </select> */}
+          <Controller
+            name="favouriteFruit"
+            control={control}
+            render={({ field }) => (
+              <Select
+                {...field}
+                options={[
+                  { value: "lime", label: "lime" },
+                  { value: "strawberry", label: "Strawberry" },
+                  { value: "vanilla", label: "Vanilla" },
+                ]}
+              />
+            )}
+          />
         </label>
-        <br />
-        <input type="submit" value="Save to model" />
+
+        <input type="submit" value="Save to model"></input>
       </form>
-    </>
+    </div>
   );
 }
+
+export default App;
