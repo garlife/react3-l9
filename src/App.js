b7cf4f96c7639b52f4540bc24fa97c6bf801174b
garlife <garlife@gmail.com>
2022-01-29 12:01:00 +0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import React from 'react';
 
-const Data = () => (
-  <>
-    <h1>Ссылки</h1>
-    <label>Текст</label>
-    <input type="text" ref={this.myRef} />
-  </>
-);
+const App = () => {
+  const myRef = useRef();
 
-const WrapperData = React.forwardRef(
-  (props, ref) => (
+  return (
     <>
       <h1>Ссылки</h1>
-      <label>Текст</label>
-      <input type="text" ref={ref} />
+      <label>Текст </label>
+      <input type="text" ref={myRef} />
     </>
-  )
-);
-
-export default class App extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.myRef = React.createRef();
-  }
-
-  componentDidMount() {
-    this.myRef.current.focus();
-  }
-
-  render() {
-    return (
-      <>
-        <WrapperData ref={this.myRef} />
-      </>
-    );
-  }
-}
+  );
+};
+export default App;
---
+++ src/CreateRef.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Data = () => (
+  <>
+    <h1>Ссылки</h1>
+    <label>Текст</label>
+    <input type="text" ref={this.myRef} />
+  </>
+);
+
+
+
+const WrapperData = React.forwardRef(
+  (props, ref) => (
+    <>
+      <h1>Ссылки</h1>
+      <label>Текст</label>
+      <input type="text" ref={ref} />
+    </>
+  )
+);
+
+export default class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.myRef.current.focus();
+  }
+
+  render() {
+    return (
+      <>
+        <WrapperData ref={this.myRef} />
+      </>
+    );
+  }
+}
